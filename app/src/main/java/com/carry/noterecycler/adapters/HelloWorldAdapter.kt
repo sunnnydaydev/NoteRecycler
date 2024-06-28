a313af6bc6e21e906755ae97b4dce61c1824fd32
SunnyDay <1246771571@qq.com>
2024-06-28 15:17:58 +0800

    通过xml指定LayoutManager

--- app/src/main/java/com/carry/noterecycler/adapters/HelloWorldAdapter.kt
+++ app/src/main/java/com/carry/noterecycler/adapters/HelloWorldAdapter.kt
@@ -1,19 +1,12 @@
 package com.carry.noterecycler.adapters
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
-import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.carry.noterecycler.R
 import com.carry.noterecycler.databinding.LayoutHelloWorldAdapterBinding
-import com.carry.noterecycler.helper.BindListItem
-import kotlin.properties.ReadOnlyProperty
-import kotlin.reflect.KProperty
 
 /**
  * Create by SunnyDay /06/21 22:28:50
@@ -22,20 +15,21 @@ data class HelloWorldAdapter(
     val data: List<String>
 ) : Adapter<HelloWorldViewHolder>() {
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HelloWorldViewHolder {
-        Log.d("HelloWorldAdapter","onCreateViewHolder parent->${parent}")
-        val rootView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.layout_hello_world_adapter, parent, false)
-        Log.d("HelloWorldAdapter","onCreateViewHolder-> rootView $rootView")
-        return HelloWorldViewHolder(rootView)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: LayoutHelloWorldAdapterBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.layout_hello_world_adapter,
+            parent,
+            false
+        )
+        return HelloWorldViewHolder(binding)
     }
 
     override fun getItemCount() = data.size
 
     override fun onBindViewHolder(holder: HelloWorldViewHolder, position: Int) {
-        Log.d("HelloWorldAdapter","onBindViewHolder->mBinding:${holder.mBinding}")
-        holder.mBinding.run {
+        holder.binding.run {
             tvText.text = data[position]
         }
     }
@@ -45,6 +39,5 @@ data class HelloWorldAdapter(
     }
 }
 
-data class HelloWorldViewHolder(val view: View) : ViewHolder(view) {
-    val mBinding :LayoutHelloWorldAdapterBinding by BindListItem(view)
-}
+data class HelloWorldViewHolder(val binding: LayoutHelloWorldAdapterBinding) :
+    ViewHolder(binding.root)
