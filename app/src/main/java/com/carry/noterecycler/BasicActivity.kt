4a2c316a0c41dc8e06bfc963c21786c49abd9f90
SunnyDay <1246771571@qq.com>
2024-07-31 22:18:22 +0800

    new page

---
+++ app/src/main/java/com/carry/noterecycler/BasicActivity.kt
@@ -0,0 +1,25 @@
+package com.carry.noterecycler
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.carry.noterecycler.databinding.ActivityBasicBinding
+import com.carry.noterecycler.databinding.ActivityMainBinding
+
+class BasicActivity : AppCompatActivity() {
+    private val mBinding:  ActivityBasicBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_basic)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
